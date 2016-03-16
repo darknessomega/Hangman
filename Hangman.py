e36f0f411e132c66ff58d84b9650ac867694e4bb
DarknessOmega <ylomega@gmail.com>
2016-03-16 11:50:56 -0400

    testing offine

--- Hangman.py
+++ Hangman.py
@@ -1,4 +1,19 @@
+import sqlite3 as sql
+import random
 from Tkinter import *
+
+conn = sql.connect('dictionary.db')
+conn.text_factory = str
+cur = conn.cursor()
+
+print cur.execute('SELECT word, pos, def FROM eng_dict WHERE word LIKE "%Abaist"')
+                  
+all_rows = cur.fetchall()
+print('1):', all_rows)
+
+conn.close()
+
+
 root = Tk()
 root.title("Modified Hangman")
 
@@ -8,21 +23,25 @@ top_frame.pack(side = TOP, fill = BOTH, expand = YES)
 middle_frame = Frame(root)
 middle_frame.pack(side = TOP, fill = BOTH, expand = YES)
 
-##Bottom Frame BEGIN##
+##Entry Frame BEGIN##
 
 bottom_frame = Frame(root, bg = 'yellow')
 bottom_frame.pack(side = BOTTOM, fill = BOTH, expand = YES)
+
 bottom_frame_label = Label(bottom_frame, anchor = S, text = 'Entry')
 bottom_frame_label.pack()
 
 word_contained = Entry(bottom_frame)
-word_contained.pack()
+word_contained.pack(side = LEFT, anchor = S, padx = 1, pady = 1)
 
-word_contained_button = Button(bottom_frame, text = 'Submit')
-word_contained_button.pack(side = BOTTOM, anchor = E)
+def retrieve_input():
+    user_word = word_contained.get("1.0", 'end-1c')
+
+word_contained_button = Button(bottom_frame, text = 'Submit', command = retrieve_input)
+word_contained_button.pack(side = LEFT, anchor = S, pady = 1)
                                                      
 
-##Bottom Frame END##
+##Entry Frame END##
 
 ##Word Frame BEGIN##
 
@@ -30,6 +49,9 @@ word_frame = Frame(top_frame, bg = 'red')
 word_frame.pack(side = LEFT, fill = BOTH, expand = YES)
 word_frame_label = Label(word_frame, text = 'Word')
 word_frame_label.pack()
+
+mystery_word = Text (word_frame, height = 4)
+mystery_word.pack()
 
 ##Word Frame END##
 
