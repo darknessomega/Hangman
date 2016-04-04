fd03caffc76fa2202f9fd30dbc887bedcbef0933
DarknessOmega <ylomega@gmail.com>
2016-04-03 23:38:47 -0400

    Correct letter behavior coded, dupication of widget persists

--- Hangman.py
+++ Hangman.py
@@ -38,32 +38,91 @@ def search_dict():
     cur.execute('SELECT word FROM eng_dict WHERE def LIKE ?', ('%' + test_word + '%',))
     return cur.fetchall()
 
-def select_word():
+game_word = ['']
+def get_game_word(game_word):
     search_results = search_dict()
     game_word_tuple = random.choice(search_results)
-    game_word = string.join(game_word_tuple)
+    game_word[0] = string.join(game_word_tuple).lower()[3:]
     return game_word
 
-def get_game_word():
-    word = select_word().lower()[3:]
-    return word
 
-def letter_spaces():
-    word = get_game_word()
-    spaces = ''
-    for i in range(len(word)):
-        spaces += '_ '
-    spaces = spaces[:-1]
-    word_frame_game_word = Label(word_frame, font = 40, text = spaces, bg = 'white')
-    word_frame_game_word.pack(padx = 4, pady = 4)
+correct_letter_indices = []
+wrong_letters = []
+labelText = StringVar()
+ils_flag = True
+def initial_letter_space(labelText, ils_flag):
+    letter_spaces = []
+    for i in range(len(game_word[0])):
+        letter_spaces += '_'
+    string_letter_spaces = string.join(letter_spaces)
+    labelText.set(string_letter_spaces)
+    if (ils_flag == True):
+        word_frame_game_word = Label(word_frame, font = 40, textvariable = labelText , bg = 'white')
+        word_frame_game_word.pack(padx = 4, pady = 4)
+    ils_flag = False
+    return letter_spaces
+
+ 
+
     
+def letter_guess():
+    return_letter = letter_guess_entry.get()
+    return return_letter
+
+def match_check(indices, letters):
+    match = False
+    guess = letter_guess()
+    for i in range(len(game_word[0])):
+        if(guess == game_word[0][i]):
+            match = True
+            indices.append(i)
+            if(i == len(game_word[0])):
+                return indices
+    if(match == False):
+        print guess
+        letters.append(guess)
+        print string.join(letters)
+        return letters
+
+letter_spaces = []
+els_flag = True
+
+def edit_letter_space(indices, letter_spaces, els_flag):
+    if (els_flag == True):
+        letter_spaces = initial_letter_space(labelText, ils_flag)
+        els_flag = False
+    for i in indices:
+        letter_spaces[i] = game_word[0][i]
+    print letter_spaces
+    labelText.set(string.join(letter_spaces))
+    return letter_spaces
+
+
+
+def letter_guess_button_cmd():
+    letter_guess()
+    match_check(correct_letter_indices, wrong_letters)
+    letters_frame_tries = Label(letters_frame, font = 40, text = string.join(wrong_letters), bg = 'white')
+    letters_frame_tries.pack(padx = 4, pady = 4)
+    if(len(wrong_letters) > 1):
+        letters_frame_tries.pack_forget()
+    edit_letter_space(correct_letter_indices, letter_spaces, els_flag)
+    
+
+        
+
+letter_guess_entry = Entry(bottom_frame)
+letter_guess_button = Button(bottom_frame, text = 'Submit letter guess', command = letter_guess_button_cmd)
+
 def word_contained_button_cmd():
-    game_word = get_game_word()
-    letter_spaces()
-    print game_word
+    get_game_word(game_word)
+    initial_letter_space(labelText, ils_flag)
+    print game_word[0]
+    word_contained_entry.pack_forget()  
+    letter_guess_entry.pack(side = LEFT, anchor = S, padx = 1, pady = 1)
     word_contained_button.pack_forget()
-    letter_guess_button = Button(bottom_frame, text = 'Submit letter guess')
     letter_guess_button.pack(side = LEFT, anchor = S, pady = 1)
+    
 
 word_contained_button = Button(bottom_frame, text = 'Submit search word', command = word_contained_button_cmd)
 word_contained_button.pack(side = LEFT, anchor = S, pady = 1)
@@ -106,6 +165,13 @@ letters_frame = Frame(middle_frame, bg = 'orange')
 letters_frame.pack(side = RIGHT, fill = BOTH, expand = YES)
 letters_frame_label = Label(letters_frame, text = 'Letters Tried')
 letters_frame_label.pack()
+
+
+
+
+
+
+
 
 ##Letters Frame END#
 
